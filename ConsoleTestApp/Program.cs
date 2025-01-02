df317bd88c6b35d3b8ff571adc790ac3633ae442
Louis Yang <12879642+louisyang2015@users.noreply.github.com>
2025-01-01 21:26:13 -0800

    Added Histogram.Get_GMM_Estimate()
    Added Histogram.Get_GMM_Estimate()
    
    Returns double[] mean, variance, weights so to provide in initial guess for the EM algorithm

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -6,13 +6,15 @@ using static JXMath.Globals;
 
 
 
-// Using "Fit" directly require good in itial guess
+// Generate test data
 var rv = new RandomVariableSet();
 rv.Add_NormalRV(0.3, 1, 1);
 rv.Add_NormalRV(0.7, 4, 2);
 
 var data = rv.Sample(10000);
 
+// Example Part 1
+// Using "Fit" directly require good in initial guess
 var model = new GaussianMixtureModel([1, 4], [1, 4], [0.3, 0.7]);
 
 model.Debug = true;
@@ -20,10 +22,25 @@ model.Debug = true;
 model.Fit(data);
 
 
+// Example Part 2 - using ".MultipleFits()" still require approximate
+// initial guess
 WriteLine("\nUsing 'MultipleFits()'");
 
 var model2 = new GaussianMixtureModel([0, 6], [1, 1], [0.5, 0.5]);
 model2.Debug = true;
 model2.TerminationTolerance = 1e-7;
 model2.MultipleFits(data);
+
+
+// Example Part 3 - use histogram to get an estimate first.
+// But sometimes 3 components are found (instead of 2).
+WriteLine("\nUsing histogram to get an estimate first.");
+var histogram = new Histogram(data);
+var estimates = histogram.Get_GMM_Estimate();
+
+var model3 = new GaussianMixtureModel(estimates.mean, estimates.variance, estimates.weights);
+model3.Debug = true;
+model3.TerminationTolerance = 1e-7;
+model3.MultipleFits(data);
+
 
--- JXMath/Histogram.cs
+++ JXMath/Histogram.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static System.Math;
+using static JXMath.Globals;
+
+
 namespace JXMath
 {
     public class Histogram
@@ -141,7 +145,7 @@ namespace JXMath
 
 
 
-        #region Implementation for double[]
+        #region Implementation for byte[,]
 
         byte[,] _data_bytes_2d = new byte[0, 0];
 
@@ -199,6 +203,182 @@ namespace JXMath
                     _histogram[bin]++;
                 }
             }
+        }
+
+        #endregion
+
+
+        #region GMM Estimation
+
+        /// <summary>
+        /// Returns arrays of (mean, variance, weight) estimates for Gaussian 
+        /// Mixture Model using the vertical sweep method.
+        /// </summary>
+        /// <param name="minGap">This is the distance between a peak and a nearby 
+        /// different interval. The unit for this distance is the number of bins.</param>
+        public (double[] mean, double[] variance, double[] weights) Get_GMM_Estimate(
+            int? minGap = null)
+        {
+            // Minimum gap between intervals is 5% of the total number of bins
+            int min_gap = (int)(_histogram.Length / 100.0 * 5.0);
+            if (min_gap < 1)
+                min_gap = 1;
+
+            if (minGap.HasValue)
+                min_gap = minGap.Value;
+
+            // Clone the histogram -> y_values
+            // Sort the y_values from largest to smallest
+            var y_values = new int[_histogram.Length];
+            Array.Copy(_histogram, y_values, _histogram.Length);
+            Array.Sort(y_values);
+            Array.Reverse(y_values);
+
+            // Each bin gets an "interval" number
+            var interval = new short[_histogram.Length];
+
+            // Initially assign all bins to an interval number of -1
+            for (int i = 0; i < interval.Length; i++)
+                interval[i] = -1;
+
+            // Vertical scan "_histogram"
+            short num_intervals = 0;
+
+            // Track how many bins and values remain unassigned
+            int bins_left = _histogram.Length;
+            int total_values = _histogram.Sum();
+            int values_left = total_values;
+
+            for (int i = 0; i < y_values.Length; i++)
+            {
+                // Check for termination if the "y_value" has dropped sufficiently low
+                double avg_height = (double)values_left / bins_left;
+
+                if (y_values[i] < avg_height * 2)
+                    break;
+
+                // Handle all bins with y_values[i]
+                for (int j = 0; j < interval.Length; j++)
+                {
+                    if (_histogram[j] == y_values[i])
+                    {
+                        // Assign the bin to an interval
+                        var result = AssignBinToInterval(interval, j, min_gap, ref num_intervals);
+                        bins_left -= result.binsAssigned;
+                        values_left -= result.valuesAssigned;
+                    }
+                }
+            }
+
+            // Compute mean and variance for the intervals
+            var means = new double[num_intervals];
+            var variances = new double[num_intervals];
+            var weights = new double[num_intervals];
+
+            for (int i = 0; i < num_intervals; i++)
+            {
+                var stats = Compute_Stats_for_Interval(interval, i);
+                means[i] = stats.mean;
+                variances[i] = stats.variance;
+                weights[i] = (double)stats.numValues / total_values;
+            }
+
+            weights.Scale(1.0 / weights.Sum());
+
+            return (means, variances, weights);
+        }
+
+
+        /// <summary>
+        /// Assign interval[index] to an interval number. If the interval[index]
+        /// is at least "minGap" away from the closest interval, then assign 
+        /// a new interval number. Returns the number of bins and values assigned.
+        /// </summary>
+        (int binsAssigned, int valuesAssigned) AssignBinToInterval(short[] interval, 
+            int index, int minGap, ref short numIntervals)
+        {
+            // If the position is already assigned to an interval, then return
+            if (interval[index] != -1)
+                return (0, 0);
+
+            int values_assigned = 0;
+
+            // See if there's other intervals nearby
+            for (int i = 0; i <= minGap; i++)
+            {
+                if (index + i < interval.Length
+                    && interval[index + i] != -1)
+                {
+                    // interval[index ... index + i] is the same interval
+                    for (int j = 0; j < i; j++)
+                    {
+                        interval[index + j] = interval[index + i];
+                        values_assigned += _histogram[index + j];
+                    }
+
+                    // bins_assigned = i;
+                    return (i, values_assigned);
+                }
+
+                if (index - i >= 0
+                    && interval[index - i] != -1)
+                {
+                    // interval[index - i ... index] is the same interval
+                    for (int j = 0; j < i; j++)
+                    {
+                        interval[index - j] = interval[index - i];
+                        values_assigned += _histogram[index + j];
+                    }
+
+                    // bins_assigned = i;
+                    return (i, values_assigned);
+                }
+            }
+
+            // Code arrives here if the point at "index" is a new interval
+            interval[index] = numIntervals;
+            numIntervals++;
+
+            return (1, _histogram[index]);
+        }
+
+
+        /// <summary>
+        /// Compute mean and variance for a certain interval
+        /// </summary>
+        (double mean, double variance, int numValues) Compute_Stats_for_Interval(
+            short[] interval, int intervalNumber)
+        {
+            // Determine the mean first
+            double total = 0;
+            double bin_width = BinWidth;
+            int num_values = 0;
+
+            for (int i = 0; i < interval.Length; i++)
+            {
+                if (interval[i] == intervalNumber)
+                {
+                    total += _histogram[i] * (Low + i * bin_width);
+                    num_values += _histogram[i];
+                }
+            }
+
+            double mean = total / num_values;
+
+            // Determine the variance
+            total = 0;
+
+            for (int i = 0; i < interval.Length; i++)
+            {
+                if (interval[i] == intervalNumber)
+                {
+                    total += _histogram[i] * Pow((Low + i * bin_width) - mean, 2);
+                }
+            }
+
+            double variance = total / num_values;
+
+            return (mean, variance, num_values);
         }
 
         #endregion
